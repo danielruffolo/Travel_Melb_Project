21a98df9ce30d146f6c85999f1b165a2d063b5f2
cwhurley <cwhurley94@gmail.com>
2017-04-04 16:17:40 +1000

    Menu Created

---
+++ Travel_Melb_Project/TimeTableVC.swift
@@ -0,0 +1,37 @@
+//
+//  TimeTableVC.swift
+//  Travel_Melb_Project
+//
+//  Created by CHRIS HURLEY on 4/04/2017.
+//  Copyright © 2017 Travel_Melb_Team. All rights reserved.
+//
+
+import UIKit
+
+class TimeTableVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addSlideMenuButton()
+        self.title = "TimeTable"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Travel_Melb_Project/ViewController.swift
+++ Travel_Melb_Project/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.addSlideMenuButton()
+        //self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+        
+        self.navigationController?.navigationBar.isTranslucent = true;
+        self.navigationController?.view.backgroundColor = UIColor.clear;
+        self.navigationController?.navigationBar.backgroundColor = UIColor.clear;
+        
+        
+//        self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+//            forBarMetrics:UIBarMetricsDefault];
+//        self.navigationController.navigationBar.shadowImage = [UIImage new];
+//        self.navigationController.navigationBar.translucent = YES;
+//        self.navigationController.view.backgroundColor = [UIColor clearColor];
+//        self.navigationController.navigationBar.backgroundColor = [UIColor clearColor];
     }
 
     override func didReceiveMemoryWarning() {
