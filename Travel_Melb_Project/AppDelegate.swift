eb8c6389e151c2b863985adb2cdb5c7faefb36ec
druffolo <ruffod17@gmail.com>
2017-04-04 18:17:51 +1000

    Google Maps Integrated

--- Travel_Melb_Project/AppDelegate.swift
+++ Travel_Melb_Project/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        GMSServices.provideAPIKey("AIzaSyALdEH3m12Vh-8KHodJvZHtXBiFMvPx6bw")
+        
+        
         // Override point for customization after application launch.
         return true
     }
--- Travel_Melb_Project/ViewController.swift
+++ Travel_Melb_Project/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ViewController: BaseViewController {
 
     override func viewDidLoad() {
