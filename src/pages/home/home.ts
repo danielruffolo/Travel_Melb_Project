99dc723adcfa43bf1952ba560af1de87c4a83835
DansMacBook <druffolo@deakin.edu.au>
2017-05-10 17:55:46 +1000

    New commit
    This is new code

---
+++ src/pages/home/home.ts
@@ -0,0 +1,34 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+@ViewChild('map') mapElement;
+map: any;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad(){
+    this.initMap();
+  }
+
+  initMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+
+}
